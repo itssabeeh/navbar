babe697c680d2d8236d86abcd02876f2f442630e
layone <itssabeeh@gmail.com>
2021-02-10 21:35:00 +0530

    completed

---
+++ src/Navbar.js
@@ -0,0 +1,56 @@
+import React, { useState, useRef, useEffect } from "react";
+import logo from "./logo.svg";
+import { FaBars } from "react-icons/fa";
+import { links, social } from "./data";
+
+const Navbar = () => {
+  const [showLinks, setShowLinks] = useState(false);
+  const linksContainerRef = useRef(null);
+  const linksRef = useRef(null);
+  const toggleLinks = () => {
+    setShowLinks(!showLinks);
+  };
+  useEffect(() => {
+    if (showLinks) {
+      const linksHeight = linksRef.current.getBoundingClientRect().height;
+      linksContainerRef.current.style.height = `${linksHeight}px`;
+    } else {
+      linksContainerRef.current.style.height = "0px";
+    }
+  }, [showLinks]);
+  return (
+    <div>
+      <navbar className="navbar">
+        <article className="navbar-header">
+          <img src={logo} alt="logo"></img>
+          <button className="toggle-button" onClick={toggleLinks}>
+            <FaBars />
+          </button>
+        </article>
+        <article className="links-container" ref={linksContainerRef}>
+          <ul className="links" ref={linksRef}>
+            {links.map((link) => {
+              const { id, url, text } = link;
+              return (
+                <li key={id}>
+                  <a href={url}>{text}</a>
+                </li>
+              );
+            })}
+          </ul>
+        </article>
+        <ul className="social-icons">
+          {social.map((socialIcon) => {
+            const { id, url, icon } = socialIcon;
+            return (
+              <li key={id}>
+                <a href={url}>{icon}</a>
+              </li>
+            );
+          })}
+        </ul>
+      </navbar>
+    </div>
+  );
+};
+export default Navbar;
